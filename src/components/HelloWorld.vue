4054cb86b0dfb61737fdac4ffbc9e7fe2ef7d7f7
jiangxi <1264005131@qq.com>
2019-02-27 14:57:14 +0800

    delete useless code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="hello">
-    <p>我是master分支</p>
-    <h1>冲突了吧</h1>
-    <h1>哎哟喂</h1>
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <el-radio-group v-model="radio3">
-      <el-radio-button label="0"><i class="el-icon-star-on" style="font-size: 18px;">差</i></el-radio-button>
-      <el-radio-button label="1"><i class="el-icon-star-on" style="font-size: 18px;">中</i></el-radio-button>
-      <el-radio-button label="2"><i class="el-icon-star-on" style="font-size: 18px;">好</i></el-radio-button>
-    </el-radio-group>
-    <p>天下之大，无奇不有</p>
-    <h1>世间万物，必有始终</h1>
+    <p>我是dev分支</p>
   </div>
 </template>
 
@@ -20,36 +9,18 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      value3: null,
-      texts: ['差', '较差', '一般', '满意', '非常满意'],
-      radio3: '0'
+      msg: 'Welcome to Your Vue.js App'
     }
   },
   methods: {
-    handleChange (score) {
-      debugger
-    }
+    
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.el-radio-button__inner {
-  border: none!important;
-  padding: 10px;
-}
-.el-radio-button__inner:hover {
-    color: #F7BA2A;
-}
-.el-radio-button__orig-radio:checked+.el-radio-button__inner {
-    color: #F7BA2A;
-    background-color: #fff;
-    border-color: #fff;
-    -webkit-box-shadow: none!important;
-    box-shadow: none!important;
-}
+
 </style>
 
 <style scoped>
