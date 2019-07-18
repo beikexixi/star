03b30236b17418473c47bb28365ed633dd6474bd
jiangxi <1264005131@qq.com>
2019-07-18 17:58:00 +0800

    update

--- src/components/动态生成多个选择器.vue
+++ src/components/动态生成多个选择器.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="hello">
     <el-form>
-      <!-- <el-select :placeholder="item.name" v-for="(item, index) in t" value-key="id" :key="item.id" v-model="form.select[index]" multiple>
+      <el-select :placeholder="item.name" v-for="(item, index) in t" value-key="id" :key="item.id" v-model="form.select[index]" multiple>
         <el-option v-for="subItem in item.taskTypeDtos" :key="subItem.id" :label="subItem.name" :value="subItem"></el-option>
-      </el-select> -->
-      <el-select :placeholder="item.name" v-for="(item, index) in t" :key="item.id" v-model="form.select[index]" multiple>
-        <el-option v-for="subItem in item.taskTypeDtos" :key="subItem.id" :label="subItem.name" :value="subItem.id"></el-option>
       </el-select>
+      <!-- <el-select :placeholder="item.name" v-for="(item, index) in t" :key="item.id" v-model="form.select[index]" multiple>
+        <el-option v-for="subItem in item.taskTypeDtos" :key="subItem.id" :label="subItem.name" :value="subItem.id"></el-option>
+      </el-select> -->
       <el-button @click="handleSubmit">提交</el-button>
     </el-form>
   </div>
@@ -22,44 +22,25 @@ export default {
       t: [
         {
           "id": 1,
-          "name": "类型_0",
+          "name": "选项0",
           "taskTypeDtos": [
-            { "id": 1,
-              "name": "类型_0"
-            },
             {
               "id": 10,
-              "code": null,
-              "name": "技能_10",
-              "skillName": null,
-              "pid": null,
-              "pcode": null
+              "name": "香蕉",
             },
             {
               "id": 11,
-              "code": null,
-              "name": "技能_11",
-              "skillName": null,
-              "pid": null,
-              "pcode": null
+              "name": "葡萄",
             }
           ]
         },
         {
           "id": 2,
-          "name": "类型_1",
+          "name": "选项1",
           "taskTypeDtos": [
             {
-              "id": 2,
-              "name": "类型_1",
-            },
-            {
               "id": 20,
-              "code": null,
-              "name": "技能_20",
-              "skillName": null,
-              "pid": null,
-              "pcode": null
+              "name": "白菜",
             }
           ]
         }
